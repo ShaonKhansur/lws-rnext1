6e4d022b278f7efb807378c86e7d8763ba991736
shaon <shaonkhansur@gmail.com>
2024-01-18 22:48:13 +0600

    init

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ export default function Navbar() {
       <div className="container mx-auto flex items-center justify-between gap-x-6">
         {/* Logo */}
         <a href="/">
-          <img className="h-[40px]" src="src/assets/lws-logo-en.svg" alt="Lws" />
+          <img className="h-[40px]" src="assets/lws-logo-en.svg" alt="Lws" />
         </a>
         {/* <!-- Logo Ends --> */}
         <a className="px-5 py-2 bg-[#172227] rounded-[44px]" href="#">
